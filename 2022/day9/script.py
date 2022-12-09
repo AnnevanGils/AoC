12321a943ad9f2b66ad962bb870730ee5e791887
AnnevanGils <amhvgils@outlook.com>
2022-12-09 15:44:33 +0100

    day 9

---
+++ 2022/day9/script.py
@@ -0,0 +1,117 @@
+tail_visited = []
+import numpy as np
+
+def catch_up_tail_compact(pos_head_new, pos_tail_old):
+    v_tail = pos_tail_old[0]
+    h_tail = pos_tail_old[1]
+    # check if on same axis
+    same_axis = (pos_head_new[0] == pos_tail_old[0] or pos_head_new[1] == pos_tail_old[1])
+
+    sign_v = np.sign(pos_head_new[0] - pos_tail_old[0])
+    sign_h = np.sign(pos_head_new[1] - pos_tail_old[1])
+    diff_v = abs(pos_head_new[0] - pos_tail_old[0])
+    diff_h = abs(pos_head_new[1] - pos_tail_old[1])
+
+    # update per axis
+    # vertical axis
+    if(diff_v > 1):
+        v_tail += sign_v * diff_v -1 * sign_v
+        if not same_axis:
+            # determine direction for stepping h_tail to
+            # if head left from tail, move h_tail to left
+            # if head right from tail, move h_tail to right
+            h_tail += sign_h
+
+    # horizontal axis
+    elif(diff_h > 1):
+        h_tail += sign_h * diff_h -1 * sign_h
+        if not same_axis:
+            v_tail += sign_v
+    
+    return [v_tail, h_tail]
+
+
+def catch_up_tail(pos_head_new, pos_tail_old):
+    # check if head and tail are on the same axis
+    if(pos_head_new[0] == pos_tail_old[0]):
+        # head and tail are in same row
+        # take step with tail if distance between tail and head larger than 1
+        # also use this difference to find the direction in which to change tail
+        if(pos_head_new[1] - pos_tail_old[1] > 1):
+            # head is right from tail, move tail one to the right
+            return (pos_tail_old[0], pos_tail_old[1] + 1)
+        elif(pos_head_new[1] - pos_tail_old[1] < -1):
+            # head is left from tail, move tail one to the left
+            return (pos_tail_old[0], pos_tail_old[1] - 1)
+        else:
+            # tail is not moved
+            return pos_tail_old
+    elif(pos_head_new[1] == pos_tail_old[1]):
+        # head and tail are in same column
+        # take step with tail if distance between tail and head larger than 1
+        # also use this difference to find the direction in which to change tail
+        if(pos_head_new[0] - pos_tail_old[0] > 1):
+            # head is up from tail, move tail one up
+            return (pos_tail_old[0] + 1, pos_tail_old[1])
+        elif(pos_head_new[0] - pos_tail_old[0] < -1):
+            # head is down from tail, move tail one down
+            return (pos_tail_old[0] - 1, pos_tail_old[1])
+        else:
+            # tail is not moved
+            return pos_tail_old
+    else:
+        # head and tail are not on same axis
+        pass
+
+
+
+with open("day9/input.txt", 'r') as f:
+    pos_head = [0,0]
+    pos_tail = [0,0]
+    tail_visited.append(pos_tail)
+    for line in f.readlines():
+        direction, steps = line.strip("\n").split(" ")
+        steps = int(steps)
+        
+        for i in range(steps):
+            if(direction == 'R'):
+                pos_head[1] += 1
+            elif(direction == 'U'):
+                pos_head[0] += 1
+            elif(direction == 'D'):
+                pos_head[0] -= 1
+            elif(direction == 'L'):
+                pos_head[1] -= 1
+
+            pos_tail = catch_up_tail_compact(pos_head, pos_tail)
+            tail_visited.append(pos_tail)
+
+tail_visited_str = ["_".join([str(s) for s in a]) for a in tail_visited]
+print(len(set(tail_visited_str)))
+
+# part 2
+tail_visited_2 = []
+with open("day9/input.txt", 'r') as f:
+    pos_nodes = [[0,0] for i in range(10)]
+    tail_visited_2.append(pos_nodes[-1])
+    for line in f.readlines():
+        direction, steps = line.strip("\n").split(" ")
+        steps = int(steps)
+        
+        for i in range(steps):
+            if(direction == 'R'):
+                pos_nodes[0][1] += 1
+            elif(direction == 'U'):
+                pos_nodes[0][0] += 1
+            elif(direction == 'D'):
+                pos_nodes[0][0] -= 1
+            elif(direction == 'L'):
+                pos_nodes[0][1] -= 1
+            
+            for n, node in enumerate(pos_nodes[1:]):
+                pos_nodes[n+1] = catch_up_tail_compact(pos_nodes[n], node)
+
+            tail_visited_2.append(pos_nodes[-1])
+
+tail_visited_2_str = ["_".join([str(s) for s in a]) for a in tail_visited_2]
+print(len(set(tail_visited_2_str)))
